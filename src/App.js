53bbda63fb87bf0539d7c7ae2c6a0b23794844f5
hoon <hoon@hoon-MacBookPro.local>
2021-01-12 11:25:04 +0900

    typescript template

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>yourang</div>;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>ok</div>;
+}
+
+export default App;

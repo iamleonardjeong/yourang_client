cbeb1fdff1ea2747765d91e62ba71b7cd4b13068
ahcock <63637115+ahcock@users.noreply.github.com>
2021-02-01 00:21:08 +0900

    merged with upstream master

--- src/components/EditInfo.tsx
+++ src/components/EditInfo.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
-import "../styles/EditInfo.scss";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
+import React, { useEffect, useState, useRef } from 'react';
+import '../styles/EditInfo.scss';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 type EditInfoProp = {
   editOnModal: () => void;
@@ -16,13 +16,13 @@ type EditInfoProp = {
 };
 
 function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
-  const authorization = localStorage.getItem("authorization");
+  const authorization = localStorage.getItem('authorization');
   const [inputForm, setInputForm] = useState({
-    editEmail: "",
-    currentPassword: "",
-    password: "",
-    inspect: "",
-    editPhone: "",
+    editEmail: '',
+    currentPassword: '',
+    password: '',
+    inspect: '',
+    editPhone: '',
   });
 
   const history = useHistory();
@@ -37,7 +37,7 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
   } = inputForm;
   const { email } = userinfo;
 
-  const [img, setImage] = useState<any>("");
+  const [img, setImage] = useState<any>('');
   const [file, setFile] = useState<any>(null);
 
   const fileChangedHandler = (e: any) => {
@@ -59,16 +59,16 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
   const onClickHandler = (e: any) => {
     let form = new FormData();
     // const authorization = localStorage.getItem("authorization");
-    form.append("image", file, file.name);
-    console.log("파일의 형식", form);
+    form.append('image', file, file.name);
+    console.log('파일의 형식', form);
     axios
-      .post("http://yourang-server.link:5000/user/modify-photo", form, {
+      .post('http://yourang-server.link:5000/user/modify-photo', form, {
         headers: {
           authorization: authorization,
         },
       })
       .then((res) => {
-        console.log("사진 보내기", res);
+        console.log('사진 보내기', res);
         photoChangeChecker();
       });
   };
@@ -76,8 +76,8 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
   const submitHander = () => {
     // const authorization = localStorage.getItem("authorization");
     axios({
-      url: "http://yourang-server.link:5000/user/modify-pass",
-      method: "post",
+      url: 'http://yourang-server.link:5000/user/modify-pass',
+      method: 'post',
       headers: {
         authorization: authorization,
       },
@@ -86,7 +86,7 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
         newPassword: password,
       },
     }).then((res) => {
-      console.log("비번 변경 완료", res);
+      console.log('비번 변경 완료', res);
       editOnModal();
     });
   };
@@ -103,10 +103,10 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
   const mobileInputHander = (e: React.KeyboardEvent<HTMLInputElement>) => {
     // mobile전용 유효성 검사 및 입력제한 로직(출력 예시: 000-0000-0000 숫자로만 입력됨. "-"는 자동입력 됨)
     const { name, value, maxLength } = e.currentTarget;
-    let reg = new RegExp("^[0-9]");
+    let reg = new RegExp('^[0-9]');
     if (reg.test(e.key)) {
       if (value.length === 3 || value.length === 8) {
-        setInputForm({ ...inputForm, [name]: value + "-" + e.key });
+        setInputForm({ ...inputForm, [name]: value + '-' + e.key });
       } else {
         setInputForm({
           ...inputForm,
@@ -115,11 +115,11 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
       }
     }
 
-    if (e.key === "Backspace" && value.length === 9) {
+    if (e.key === 'Backspace' && value.length === 9) {
       setInputForm({ ...inputForm, [name]: value.substring(0, 8) });
-    } else if (e.key === "Backspace" && value.length === 4) {
+    } else if (e.key === 'Backspace' && value.length === 4) {
       setInputForm({ ...inputForm, [name]: value.substring(0, 3) });
-    } else if (e.key === "Backspace") {
+    } else if (e.key === 'Backspace') {
       setInputForm({
         ...inputForm,
         [name]: value.substring(0, value.length - 1),
@@ -195,7 +195,7 @@ function EditInfo({ editOnModal, userinfo, photoChangeChecker }: EditInfoProp) {
                 작성된 비밀번호가 서로 다릅니다
               </div>
             ) : (
-              ""
+              ''
             )}
 
             <div className="editinfo_btn_container">
